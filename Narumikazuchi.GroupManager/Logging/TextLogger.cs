8edca1a86b53d0347da83cf27fe705a4086245a7
Narumikazuchi <ganzel.davy@swschwedt.de>
2022-03-23 12:16:32 +0100

    Implemented logging capabilities.

---
+++ Narumikazuchi.GroupManager/Logging/TextLogger.cs
@@ -0,0 +1,51 @@
+﻿namespace Narumikazuchi.GroupManager;
+
+public sealed class TextLogger
+{
+    private TextLogger(DirectoryInfo directory!!)
+    {
+        this.Location = directory;
+    }
+
+    public void Log([DisallowNull] String message!!)
+    {
+        String path = Path.Combine(this.Location
+                                       .FullName,
+                                   DateTime.Today
+                                           .ToString() + ".log");
+        using StreamWriter writer = File.AppendText(path);
+        writer.Write(DateTime.Now.ToString());
+        writer.Write(" === ");
+        writer.WriteLine(message);
+        writer.Flush();
+    }
+    public void Log<TException>([DisallowNull] TException exception!!)
+        where TException : Exception
+    {
+        String path = Path.Combine(this.Location
+                                       .FullName,
+                                   DateTime.Today
+                                           .ToString() + ".log");
+        using StreamWriter writer = File.AppendText(path);
+        writer.Write(DateTime.Now.ToString());
+        writer.Write(" === ");
+        writer.WriteLine(exception.ToString());
+        writer.Flush();
+    }
+
+    public static void Configure([DisallowNull] DirectoryInfo directory!!)
+    {
+        if (!directory.Exists)
+        {
+            Directory.CreateDirectory(directory.FullName);
+        }
+        s_Instance = new(directory);
+    }
+
+    public DirectoryInfo Location { get; }
+
+    public static TextLogger Instance =>
+        s_Instance;
+
+    private static TextLogger s_Instance = new(new(Environment.CurrentDirectory));
+}
